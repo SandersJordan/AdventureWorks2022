06df477063db28294e297f6682e6b43b5a75d4ee
Ekaterinakor <ekaterinakor@devart.com>
2024-12-04 11:26:21 +0200

    initial commit

---
+++ Security/Schemas/Sales.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [Sales]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to customers, sales orders, and sales territories.', 'SCHEMA', N'Sales'
+GO
